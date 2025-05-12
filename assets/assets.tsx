e047e0ecfc6d81f758d7b1bffa07f8f28be2ff20
RanjanVerma <Vermaranjan9964@gmail.com>
2025-05-12 15:43:09 +0530

    removed class img

--- assets/assets.tsx
+++ assets/assets.tsx
@@ -1,15 +1,11 @@
-import KMPSLOGO from "./logo.jpg";
-import KMPSLOGO2 from "./logo2.jpg";
+
 import KMPSLOGO3 from "./logo3.png";
 import Library from "./library.jpg";
-import computerLab from "./computer_1.jpg";
 import sports from "./sports.png";
 import Teacher1 from "./teacher1.png";
 import Teacher2 from "./teacher2.jpg";
 import Testimonial1 from "./testimonial1.jpg";
 import Testimonial2 from "./testimonial2.jpg";
-import Student1 from "./student1.jpg";
-import Student2 from "./student2.jpg";
 import SchoolCampus from "./KMPS Scholl.png";
 import KMPS1 from "./kmpsimg1.png";
 import KMPS2 from "./Kmps2.png";
@@ -27,20 +23,13 @@ import SchoolFrontView from "./School front view.jpg";
 // import SchoolFrontView from "./School front view.jpg";
 
 const assets = {
-  KMPSLOGO,
-  KMPSLOGO2,
   KMPSLOGO3,
- 
-
-  Library,
-  computerLab,
+ Library,
   sports,
   Teacher1,
   Teacher2,
   Testimonial1,
   Testimonial2,
-  Student1,
-  Student2,
   SchoolCampus,
   SchoolFrontView,
   KMPS1,
