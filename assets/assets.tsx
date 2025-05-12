709aeb56cc8a9ebe30fece5fd874dc2702d4f33a
RanjanVerma <Vermaranjan9964@gmail.com>
2025-05-12 15:50:56 +0530

    removed img1

--- assets/assets.tsx
+++ assets/assets.tsx
@@ -1,4 +1,3 @@
-
 import KMPSLOGO3 from "./logo3.png";
 import Library from "./library.jpg";
 import sports from "./sports.png";
@@ -7,24 +6,23 @@ import Teacher2 from "./teacher2.jpg";
 import Testimonial1 from "./testimonial1.jpg";
 import Testimonial2 from "./testimonial2.jpg";
 import SchoolCampus from "./KMPS Scholl.png";
-import KMPS1 from "./kmpsimg1.png";
 import KMPS2 from "./Kmps2.png";
 import KMPS_2 from "./kmpsimg2.png";
 import KMPS3 from "./kmpsimg3.png";
-import KMPS4 from "./kmpsimg1.png";
+import KMPS4 from "./kmpsimg2.png";
 import KMPS5 from "./kmpsimg5.png";
 import KMPS6 from "./Kmpsimg6.png";
 import KMPS7 from "./kmpsimg7.png";
 import KMPS8 from "./kmpsimg8.png";
 import KMPS9 from "./kmpsimg9.png";
-import KMPS10 from "./kmpsimg1.png";
-import KMPS12 from "./kmpsimg1.png";
+import KMPS10 from "./kmpsimg2.png";
+import KMPS12 from "./kmpsimg2.png";
 import SchoolFrontView from "./School front view.jpg";
 // import SchoolFrontView from "./School front view.jpg";
 
 const assets = {
   KMPSLOGO3,
- Library,
+  Library,
   sports,
   Teacher1,
   Teacher2,
@@ -32,7 +30,6 @@ const assets = {
   Testimonial2,
   SchoolCampus,
   SchoolFrontView,
-  KMPS1,
   KMPS2,
   KMPS3,
   KMPS4,
